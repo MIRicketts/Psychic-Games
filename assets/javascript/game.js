978e19506c707a688e23fa4ecc66aa1fe3a4e996
Marsden Ricketts <marsdenrickett@gmail.com>
2019-03-28 22:49:13 -0400

    ???

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// create all of my variables
+// win (number)
+// losses
+// guesses left
+// letters already guessed-array(.push)
+// computer choices-string
+// computer choices bank (array of the alphabet)
+
+var computerChoicesBank = "abcdefghijklmpnoqrstuvwxyz".split("")
+
+// create variables that reference specific parts of the page
+// $wins (document.getelementbyID(wins))
+// loses
+// guesses left
+// userguest
+// computerGuess
+// letters already guessed
+
+// create a function to restart the game
+function newGame(){
+  // reset important game variables
+  // assign computerGuess a new random letter
+  // reset the letter already guessed array
+  letterAlreadyGuessed=0;
+
+  // reset guess letter after 10
+  // write information to the page indicating a new game has begun
+}
+
+document.onkeyup = function(event){
+
+
+}
